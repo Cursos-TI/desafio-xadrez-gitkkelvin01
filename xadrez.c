dfbf261ef8236bd9710081ac877308ed1b2eeaa3
gitkkelvin01 <kelvindasilvanascimento7@gmail.com>
2025-09-16 22:43:40 +0000

    foi criado os laços for com as variaveis para a movimentação de cada uma das peças.

--- xadrez.c
+++ xadrez.c
@@ -9,6 +9,28 @@ const int casas = 5;
 
 //- Começo da Sessão do bispo.
 printf("Movimento do Bispo (diagonal):\n");
+//- Inicio do laço for para o movimento do bispo.
+for (int i = 1; i <= casas; i++) {
+    printf("O Bispo moveu-se para a posição (%d, %d)\n", i, i);
+}
+printf("\n");
 
-    return 0;
+//- Começo da Sessão da torre.
+printf("Movimento da Torre (para a direita):\n");
+
+//- Inicio do laço for para o movimento da torre.
+for (int i = 1; i <= casas; i++) {
+    printf("A torre moveu-se para a posição (%d, 0)\n", i);
+}
+printf("\n");
+//- Começo da Sessão da Rainha.
+printf("Movimento da Rainha (para a esquerda):\n");
+
+//- Inicio do laço for para o movimento da rainha.
+for (int i = casas; i >= 1; i--) {
+    printf("A Rainha moveu-se para a posicao (%d, 0)\n", i);
+    }
+printf("\n");    
+    
+return 0;
 }
